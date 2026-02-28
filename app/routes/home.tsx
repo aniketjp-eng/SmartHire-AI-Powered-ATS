6dbdfddd1405bf223752ba4e1646c02f5f232e75
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-02-28 21:43:16 +0530

    Rendered resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,14 +20,15 @@ export default function Home() {
           <h1>Track your Application and Resume Ratings</h1>
           <h2>Review your submission and check AI-powered feedback</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.length > 0 && (
-      <div className="resume-section">
-        {resumes.map((resume) => (
-        <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-)}
+      
      
       
     </main>
